d7ff19bbd48ae12474b1d865e80508d999ecc00b
Kevin Kandlbinder <kevin@kevink.dev>
2022-02-01 02:02:33 +0100

    Move API client

--- openLigaDbClient/openLigaDbClient.go
+++ openLigaDbClient/openLigaDbClient.go
@@ -25,12 +25,14 @@ import (
 	"time"
 )
 
+// Group is a OpenLigaDB-Group (aka. League)
 type Group struct {
 	GroupName    string `json:"groupName"`
 	GroupOrderID int    `json:"groupOderID"`
 	GroupID      int    `json:"groupID"`
 }
 
+// Team is a OpenLigaDB-Team
 type Team struct {
 	TeamID        int    `json:"teamId"`
 	TeamName      string `json:"teamName"`
@@ -39,37 +41,41 @@ type Team struct {
 	TeamGroupName string `json:"teamGroupName"`
 }
 
+// Result types
 const (
-	ResultHalftime = 1
-	ResultEnd      = 2
-	ResultExtended = 3
-	ResultOvertime = 4
-	ResultEleven   = 5
+	ResultHalftime = 1 // ResultHalftime is the points as of half-time-break
+	ResultEnd      = 2 // ResultEnd is the result at the end of the regular 90 minutes
+	ResultExtended = 3 // ResultExtended is the result after time extensions (this happens if the match had delays)
+	ResultOvertime = 4 // ResultOvertime is the result after overtime (which happens when there is no winner after extended time)
+	ResultEleven   = 5 // ResultEleven is the result after 11-meter-shooting (which happens when there is no winner after overtime)
 )
 
+// MatchResult is a result entry
 type MatchResult struct {
 	ResultID          int    `json:"resultID"`
 	ResultName        string `json:"resultName"`
 	PointsTeam1       int    `json:"pointsTeam1"`
 	PointsTeam2       int    `json:"pointsTeam2"`
 	ResultOrderID     int    `json:"resultOrderID"`
-	ResultTypeID      int    `json:"resultTypeID"`
+	ResultTypeID      int    `json:"resultTypeID"` // See ResultHalftime, ResultEnd, ResultExtended, ResultOvertime and ResultEleven
 	ResultDescription string `json:"resultDescription"`
 }
 
+// Goal is a goal entry
 type Goal struct {
 	GoalID         int    `json:"goalID"`
-	ScoreTeam1     int    `json:"scoreTeam1"`
-	ScoreTeam2     int    `json:"scoreTeam2"`
+	ScoreTeam1     int    `json:"scoreTeam1"` // ScoreTeam1 is team1's score after the goal
+	ScoreTeam2     int    `json:"scoreTeam2"` // ScoreTeam2 is team2's score after the goal
 	MatchMinute    int    `json:"matchMinute"`
 	GoalGetterID   int    `json:"goalGetterID"`
 	GoalGetterName string `json:"goalGetterName"`
-	IsPenalty      bool   `json:"isPenalty"`
-	IsOwnGoal      bool   `json:"isOwnGoal"`
-	IsOvertime     bool   `json:"isOvertime"`
+	IsPenalty      bool   `json:"isPenalty"`  // IsPenalty is true if the goal was scored on a penalty shot
+	IsOwnGoal      bool   `json:"isOwnGoal"`  // IsOwnGoal is true if the goal was shot by a player on their own team's goal
+	IsOvertime     bool   `json:"isOvertime"` // IsOvertime is true if this goal was scored during overtime
 	Comment        string `json:"comment"`
 }
 
+// Match combines all data on a match. The match may be planned, ongoing or finished
 type Match struct {
 	MatchID            int           `json:"matchID"`
 	MatchDateTime      string        `json:"matchDateTime"`
@@ -91,8 +97,10 @@ type Match struct {
 	NumberOfViewers interface{} `json:"numberOfViewers"`
 }
 
+// baseURL for OpenLigaDB API
 const baseURL = "https://api.openligadb.de"
 
+// sendGETRequest is a helper function for sending a GET-request to an endpoint and returning the response
 func sendGETRequest(endpoint string) ([]byte, error) {
 	client := &http.Client{}
 
@@ -111,7 +119,9 @@ func sendGETRequest(endpoint string) ([]byte, error) {
 		return nil, err
 	}
 
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
 
 	body, err := io.ReadAll(resp.Body)
 
@@ -122,6 +132,7 @@ func sendGETRequest(endpoint string) ([]byte, error) {
 	return body, nil
 }
 
+// GetMatchByID gets a specific match
 func GetMatchByID(id int) (*Match, error) {
 	body, err := sendGETRequest("/getmatchdata/" + strconv.Itoa(id))
 
@@ -139,6 +150,7 @@ func GetMatchByID(id int) (*Match, error) {
 	return &match, err
 }
 
+// GetMatchesByLeague finds all matches in a given league
 func GetMatchesByLeague(league string) ([]Match, error) {
 	body, err := sendGETRequest("/getmatchdata/" + league)
 
@@ -156,6 +168,7 @@ func GetMatchesByLeague(league string) ([]Match, error) {
 	return matches, nil
 }
 
+// ParseTime parses the timestamps from the API into a time.Time
 func ParseTime(timeStr string) (time.Time, error) {
 	myTime, err := time.Parse(time.RFC3339, timeStr)
 	return myTime, err
